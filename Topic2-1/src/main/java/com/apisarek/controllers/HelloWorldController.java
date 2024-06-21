2ba9b1ac085dcb5f799cc1eaa3fb13624d032f56
apisarek2008 <apisarek2008@gmail.com>
2024-06-21 21:47:51 +0200

    CreateTable - 21.06-21:50

--- Topic2-1/src/main/java/com/apisarek/controllers/HelloWorldController.java
+++ Topic2-1/src/main/java/com/apisarek/controllers/HelloWorldController.java
@@ -1,10 +1,16 @@
 package com.apisarek.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.apisarek.models.Person;
 
 @Controller
 public class HelloWorldController {
@@ -14,13 +20,13 @@ public class HelloWorldController {
 	public String printHello() {
 		return "Helllo World";
 	}
-	
+
 	@GetMapping("/test2")
 	public String showPrintHello(Model model) {
 		model.addAttribute("message", "Have a greate day!!!");
 		return "Hello";
 	}
-	
+
 	@GetMapping("/test3")
 	public ModelAndView printHelloAgain() {
 		ModelAndView mv = new ModelAndView();
@@ -28,6 +34,63 @@ public class HelloWorldController {
 		mv.addObject("rating", 55);
 		mv.setViewName("Hello");
 		return mv;
-		
+
 	}
+
+	@GetMapping("/test4")
+	public String showPrintHello4(@RequestParam("message") String message, Model model) {
+		model.addAttribute("message", message);
+		return "Hello";
+	}
+
+	@GetMapping("/peaple")
+	public String showFriends(Model model) {
+		
+		List<Person> friends = new ArrayList<Person>();
+		
+		friends.add(new Person (1, "Allan", 23, 56.4f));
+		friends.add(new Person (2, "Zenek", 45, 57.4f));
+		friends.add(new Person (3, "Bob", 14, 54.4f));
+		friends.add(new Person (4, "Hans", 65, 23.4f));
+		friends.add(new Person (5, "Pinokio", 33, 55.4f));
+		
+		System.out.println(friends);
+		
+		model.addAttribute("message", friends);
+		return "printFriends.html";
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
